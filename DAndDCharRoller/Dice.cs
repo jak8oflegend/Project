ac4965543a7d201915cf333089029bde0aa9bc63
thurd <thurd@B11-R111E-09>
2017-01-17 15:20:22 -0800

    I added a class to roll dice, but I can't test it.  Could you add something to one of the forms so that I can test it out?

---
+++ DAndDCharRoller/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAndDCharRoller
+{
+    class Dice
+    {
+        public static int MAX = 6;
+
+        private int DiceRoll()
+        {
+            Random rand = new Random();
+            return rand.Next(MAX) + 1;
+        }
+    }
+}
