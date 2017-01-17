578895cba1165dd9ac756ba5bfc69a4ff84505e6
jak8oflegend <jak8oflegend@gmail.com>
2017-01-17 15:04:24 -0800

    added stuff to second form not entirely done

--- DAndDCharRoller/StatRoller.Designer.cs
+++ DAndDCharRoller/StatRoller.Designer.cs
@@ -28,11 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_reroll = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_reroll
+            // 
+            this.btn_reroll.Location = new System.Drawing.Point(12, 323);
+            this.btn_reroll.Name = "btn_reroll";
+            this.btn_reroll.Size = new System.Drawing.Size(75, 23);
+            this.btn_reroll.TabIndex = 0;
+            this.btn_reroll.Text = "Reroll";
+            this.btn_reroll.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(177, 323);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 23);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(0, 39);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(100, 20);
+            this.txt_1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(0, 143);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(0, 191);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(0, 233);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox2.Location = new System.Drawing.Point(139, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox3.Location = new System.Drawing.Point(139, 189);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 11;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox4.Location = new System.Drawing.Point(139, 143);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 10;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox5.Location = new System.Drawing.Point(139, 232);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 13;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Strength",
+            "Dexterity",
+            "Constitution",
+            "Intelligence",
+            "Wisdom",
+            "Charisma"});
+            this.comboBox6.Location = new System.Drawing.Point(139, 276);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 12;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // StatRoller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 358);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_reroll);
+            this.Name = "StatRoller";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.StatRoller_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_reroll;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
     }
 }
--- DAndDCharRoller/StatRoller.cs
+++ DAndDCharRoller/StatRoller.cs
@@ -16,5 +16,118 @@ namespace DAndDCharRoller
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == comboBox2.Text) {
+                MessageBox.Show("error you have selected " + comboBox1.Text + " in 2 locations this one will be reset");
+                comboBox1.SelectedIndex = -1;
+            }
+            if (comboBox1.Text == comboBox3.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox1.Text + " in 2 locations this one will be reset");
+                comboBox1.SelectedIndex = -1;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == comboBox1.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox2.Text + " in 2 locations this one will be reset");
+                comboBox2.SelectedIndex = -1;
+            }
+            if (comboBox2.Text == comboBox3.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox2.Text + " in 2 locations this one will be reset");
+                comboBox2.SelectedIndex = -1;
+            }
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text == comboBox1.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox3.Text + " in 2 locations this one will be reset");
+                comboBox3.SelectedIndex = -1;
+            }
+            if (comboBox2.Text == comboBox3.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox3.Text + " in 2 locations this one will be reset");
+                comboBox3.SelectedIndex = -1;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.Text == comboBox1.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox4.Text + " in 2 locations this one will be reset");
+                comboBox4.SelectedIndex = -1;
+            }
+            if (comboBox4.Text == comboBox3.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox4.Text + " in 2 locations this one will be reset");
+                comboBox4.SelectedIndex = -1;
+            }
+            if (comboBox4.Text == comboBox2.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox4.Text + " in 2 locations this one will be reset");
+                comboBox4.SelectedIndex = -1;
+            }
+        }
+
+        private void StatRoller_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.Text == comboBox2.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox4.Text + " in 2 locations this one will be reset");
+                comboBox4.SelectedIndex = -1;
+            }
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox6.Text == comboBox1.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox6.Text + " in 2 locations this one will be reset");
+                comboBox6.SelectedIndex = -1;
+            }
+            if (comboBox6.Text == comboBox2.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox6.Text + " in 2 locations this one will be reset");
+                comboBox6.SelectedIndex = -1;
+            }
+            if (comboBox6.Text == comboBox3.Text)
+            {
+                MessageBox.Show("error you have selected " + comboBox6.Text + " in 2 locations this one will be reset");
+                comboBox6.SelectedIndex = -1;
+            }
+
+
+
+
+        }
     }
 }
